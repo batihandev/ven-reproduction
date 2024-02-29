80a53f58303b7b8c9ea7c3e1bed4f2ca2d5f74a8
batihandev <batihandev@gmail.com>
2024-02-29 17:50:06 +0300

    chore(nuxt.config.ts): add comment explaining the issue with autoImport option in vueEmail module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
   modules: ["@vue-email/nuxt"],
   vueEmail: {
     baseUrl: "https://vue-email-demo.vercel.app/",
+    // if true gives big int error on build if false gives error on email rendering only on build/preview dev mode works fine
     autoImport: false,
   },
   // this fixes build error but breaks the email rendering
